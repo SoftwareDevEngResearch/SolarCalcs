10c8c7e6ff910386d43e4238cfb51dd51bdaa61d
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-08 10:01:50 -0700

    Moved testing files to the tests folder

--- SolarCalcs/Test_DeclinationAngle.py
+++
@@ -1,7 +0,0 @@
-#Test the DeclinationAngle.py file
-
-import DeclinationAngle as ds
-import pytest
-
-def test_basic():
-	assert ds.declination_angle(2017, 8, 21) == pytest.approx(11.75412, rel=1e-3)
