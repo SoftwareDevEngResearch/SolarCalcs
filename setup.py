68e2398c113fab513a4900ef97a75027d74af893
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-16 13:38:40 -0700

    Created setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='SolarCalcs',
+    version='0.1.0',
+    description='Calculates  the declination, zenith angle, hour angle, azimuth angle, altitude angle, and solat time for a given location, time, and date.',
+    author='Nicole Guymer',
+    author_email='nicole@roboticmayhem.com',
+    url='',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+    ],
+    license='MIT',
+    python_requires='>=2',
+    zip_safe=False,
+    packages=['SolarCalcs', 'SolarCalcs.tests'],
+
+    package_dir={
+        'SolarCalcs': 'SolarCalcs',
+        'SolarCalcs.tests': 'SolarCalcs/tests',
+        },
+    include_package_data=True,
+
+)
