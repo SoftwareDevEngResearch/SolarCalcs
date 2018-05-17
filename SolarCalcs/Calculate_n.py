5ef0c237fb9486457b750c8dd6304b753c2392f1
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-16 20:39:37 -0700

    Added header

--- SolarCalcs/Calculate_n.py
+++ SolarCalcs/Calculate_n.py
@@ -1,4 +1,11 @@
-#Calculate n. The numbered day of the year, for example January 1st is n=1.
+#!/usr/bin/env python
+
+# Author: Nicole Guymer
+# Date: May, 16 2018
+# File: Calculate_n.py
+# Description: Calculates the numbered day of the year, for example January 1st is n=1.
+# Note: Date is in YY, MM, DD format
+
 import datetime
 import pytest
 
