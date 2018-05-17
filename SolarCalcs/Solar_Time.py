792c4b6a63f807eb9c8009e7f22d1ac5ba7b5180
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-16 18:41:39 -0700

    Got the Solar_Time.py file working properly!!

--- SolarCalcs/Solar_Time.py
+++ SolarCalcs/Solar_Time.py
@@ -1,4 +1,11 @@
-#Calculates the basic angle information
+#!/usr/bin/env python
+
+# Author: Nicole Guymer
+# Date: May, 16 2018
+# File: Solar_Time.py
+# Description: Calculates the  solar time of a given location and the standard time at that location.
+# Note: This file presents time in the 24hr format.
+
 import math
 import datetime
 import time
@@ -8,7 +15,7 @@ import LocationComponents as LC
 
 class Solar_Time():
 	"""This class calculates the solar time of the location and time given by the user.
-		Contained in this class are the calculations for LST, ET, and standard longitude.
+		Contained in this class are the calculations for LST, ET, and standard longitude (l_st).
 		These calculations pull data from Calculate_n.py and LocationComponents.py"""
 
 	def __init__(self, address, year, month, day, hour, minutes, seconds):
@@ -23,6 +30,7 @@ class Solar_Time():
 		self.minutes = int(minutes)
 		self.seconds = int(seconds)
 		time = datetime.time(self.hour , self.minutes, self.seconds)
+		self.longitude = LC.LocationComponents(self.address).longitude_value()
 
 	def B(self):
 		"""Calcluate B using data from Calculat_n.py
@@ -50,22 +58,19 @@ class Solar_Time():
 
 		#Local Standard Time
 		LST = datetime.datetime(self.year, self.month, self.day, self.hour, self.minutes, self.seconds)
-
-		long_offset = (self.l_st() - LC.LocationComponents(self.address).longitude_value())*4*60 #units = seconds
-
-
-		solar_time = LST + datetime.timedelta(seconds = self.ET()) + datetime.timedelts(seconds = long_offset)
-
-		return solar_time
-
+		long_offset = (self.l_st() - abs(self.longitude))*4*60 #units = seconds
+		return LST + datetime.timedelta(seconds = self.ET()) + datetime.timedelta(seconds = long_offset)
 
 #Function to account for day light savings, if applicable to location
 	def solar_time_daylightsavings(self):
-		return None
+		"""This function is the same as solar_time() expect that it accounts for daylight savings by subtracting an hour from the total time"""
 
-if __name__ == '__main__':
-	a = Solar_Time('4536 153rd Ave SE Bellevue, WA 98006', 2018, 5, 14, 13, 30, 0)
-	print a.solar_time()
+		#Local Standard Time
+		LST = datetime.datetime(self.year, self.month, self.day, self.hour, self.minutes, self.seconds)
+		long_offset = (self.l_st() - abs(self.longitude))*4*60 #units = seconds
+
+		#subtract 1 hour for daylight saving
+		return LST + datetime.timedelta(seconds = self.ET()) + datetime.timedelta(seconds = long_offset) - datetime.timedelta(seconds = 3600) 
 
 
 
