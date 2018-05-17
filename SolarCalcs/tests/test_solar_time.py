b3a9555dd991b1d9fc961e4e4e7e901ff3e2fe61
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-16 21:27:46 -0700

    created most of the basic tests for the Solar_Time file

--- SolarCalcs/tests/test_solar_time.py
+++ SolarCalcs/tests/test_solar_time.py
@@ -15,18 +15,25 @@ import pytest
 #----------------------------------------------------------------#
 """Tests for the B function"""
 
-a = ST.Solar_Time('Franfort, KY', 2052, 9, 19, 15, 23, 00)
+a = ST.Solar_Time('Nashville, TN', 2017, 8, 21, 13, 30, 00)
 def test_B_1():
-	return a.B()
+	assert a.B() == pytest.approx(150.33, rel=1e-2)
 
 #----------------------------------------------------------------#
 """Tests for the ET function"""
+def test_ET_1():
+	assert a.ET() == pytest.approx(-2.69*60, rel=1.e-2)
+
 
 #----------------------------------------------------------------#
 """Tests for the l_st function"""
+def test_l_st_1():
+	assert a.l_st() == int(90)
 
 #----------------------------------------------------------------#
 """Tests for the solar_time function"""
+#def test_solar_time_1():
+#	assert a.soalr_time() == pytest.approx('2017-08-21 13:40:12.749108' )
 
 #----------------------------------------------------------------#
 """Tests for the solar_time_daylightsavings function"""
