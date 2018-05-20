1c22a9b52f04211862f04920df7f262420f614ab
Nicole Guymer <nicole@roboticmayhem.com>
2018-05-19 18:01:19 -0700

    Created Hour_Angle file

---
+++ SolarCalcs/Hour_Angle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Author: Nicole Guymer
+# Date: May, 16 2018
+# File: Hour_Angle.py
+# Description: Calculates the angle hour from the given time
+# Note: This file depends on the Solar_Time.py file.
+
+from Solar_Time import ST 
+
+#find the solart time for a given time
+#calculate the mintues from solar noon
+#calculate hour angle (hs)
+
