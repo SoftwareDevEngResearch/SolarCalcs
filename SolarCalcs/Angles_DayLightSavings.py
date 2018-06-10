ac6a3862e3ab0e8da4959e2086f111987a779078
Nicole Guymer <nicole@roboticmayhem.com>
2018-06-10 13:08:06 -0700

    Added docstrings to all files.

--- SolarCalcs/Angles_DayLightSavings.py
+++ SolarCalcs/Angles_DayLightSavings.py
@@ -16,7 +16,11 @@ import datetime
 
 
 class Angles_DayLightSavings():
+
 	def __init__(self, address, year, month, day, hour, minutes, seconds):
+	"""Define all the variables in terms of the class.
+	Bring in the nth_day value from Calculate_n.py.
+	Bring in the location values from LocationComponents.py."""
 		self.address = address
 		self.year = year
 		self.month = month
@@ -30,10 +34,15 @@ class Angles_DayLightSavings():
 		self.time = datetime.time(self.hour , self.minutes, self.seconds)
 
 	def declination_angle(self):
+		"""Calculate the declination angle for the given day of the year"""
 		inside_sin = math.radians((360 * (284 + int(self.n)))/(float(365)))
 		return 23.45 * math.sin (( inside_sin) ) #returns a number with units of Degrees
 
 	def solar_time(self):
+		"""Calculate the solar time of the given location on the given time and day of the year. 
+		The local standard time (the time shown on your clock at that location) does not match the actual solar time
+		directly due to the generalization of the time zones.
+		The solar time is the true time for a given location."""
 		B = (360*( int(n.nth_day(self.year, self.month, self.day))-81) )/float(364)  #unit = degrees
 		ET = ( (float(9.87)*math.sin(2*math.radians(float(B)))) - (float(7.53)*math.cos(math.radians(float(B)))) - (float(1.5)*math.sin(math.radians(float(B)))))*60 #units = seconds
 		l_st = abs(LC.LocationComponents(self.address).time_zone()*15) #units = degrees
@@ -42,13 +51,16 @@ class Angles_DayLightSavings():
 		return LST + datetime.timedelta(seconds = ET) + datetime.timedelta(seconds = long_offset) - datetime.timedelta(seconds = 3600) 
 
 	def hour_angle(self):
-		"""This function calculates the minutes from solar noon and returns the hour angle"""
+		"""This function calculates the difference between the local time and solar noon, then returns the hour angle.
+		This file accounts for day light savings time."""
+		
 		#turn the solar time into total seconds (since midnight)
 		seconds_solartime = self.solar_time().hour*3600 + self.solar_time().minute*60 + self.solar_time().second
 		seconds_from_solar_noon = abs(seconds_solartime - 12*3600)#noon in seconds		
 		return (float(seconds_from_solar_noon)/60)/4 #units = degrees
 
 	def altitude_angle(self):
+		"""Calculate the altitude angle for the given location"""
 		a = math.sin(math.radians(self.latitude)) * math.sin(math.radians(self.declination_angle()))
 		b = math.cos(math.radians(self.latitude)) * math.cos(math.radians(self.declination_angle())) * math.cos(math.radians(self.hour_angle()))
 		c = a+b
@@ -56,14 +68,18 @@ class Angles_DayLightSavings():
 		return math.degrees(d) #units = degress
 
 	def zenith_angle(self):
+		"""Calulate the zenith angle for the given location"""
 		return 90 - self.altitude_angle()
 
 	def azimuth_angle(self):
+		"""Calculate the azimuth angle for the given time, date, and locaton"""
 		div = math.cos(math.radians(self.declination_angle())) * (math.sin(math.radians(self.hour_angle())) / math.cos(math.radians(self.altitude_angle())))
 		return math.degrees(math.asin(div))
 
 	def all_angles(self):
-		#Create a file that holds all the values
+		"""Create a text file that holds all the values from the above functions.
+		This file will have a unique name containing the time, date, and location used for the calculations."""
+
 		filename = 'SolarCalcs_DayLightSavings_%(address)s_%(year)d_%(month)d_%(day)d_%(hour)d%(minutes)d%(seconds)d.txt' % {'address': self.address[:5], 'year': self.year, 'month' :self.month, 'day': self.day, 'hour':self.hour, 'minutes': self.minutes, 'seconds':self.seconds}
 		st = str(self.solar_time())
 		with open('%s' % filename , 'w') as f:
